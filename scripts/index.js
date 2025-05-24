b19eab8ad763685b01cb134c6cd1013cf47cf5b2
AlinaScherbatiuk <alina.shcherbatiuk@yandex.ru>
2025-05-25 01:15:01 +0300

    init

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+//  Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
+//  DOM узлы
+const cardsParent = document.querySelector('.places__list');
+// Функция создания карточки
+const createCard = (item, deleteCard) => {
+    if (cardTemplate) {
+        const clone = cardTemplate.content.cloneNode(true);
+        const cardClone = clone.firstElementChild;
 
-// @todo: DOM узлы
+        const cardImage = cardClone.querySelector('.card__image');
+        const cardTitle = cardClone.querySelector('.card__title');
+        const deleteButton = cardClone.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+        if (cardImage) {
+            cardImage.src = item.link;
+            cardImage.alt = item.name;
+        }
+        if (cardTitle) {
+            cardTitle.textContent = item.name;
+        }
+        
+        cardsParent.append(cardClone);
 
-// @todo: Функция удаления карточки
+        if (deleteButton) {
+            deleteButton.addEventListener('click', deleteCard);
+        }
+    }
+}
+//  Функция удаления карточки
+const deleteCard = (event) => event.target.closest('.card').remove();
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+if (initialCards && initialCards.length > 0) {
+    initialCards.forEach(card => createCard(card, deleteCard));
+}
